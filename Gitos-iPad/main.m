8ab84396df7eec87d2e964e9ab00a9d5a889caaf
trivektor <trivektor@gmail.com>
2013-01-28 22:56:39 -0800

    Initial Commit

---
+++ Gitos-iPad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gitos-iPad
+//
+//  Created by Tri Vuong on 1/28/13.
+//  Copyright (c) 2013 Crafted By Tri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
