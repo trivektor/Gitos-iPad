d6dd076d87929aa999428f34d4f83c36c19ad8c1
trivektor <trivektor@gmail.com>
2013-02-05 21:44:39 -0800

    add ProfileWebsiteViewController

---
+++ Gitos-iPad/Controllers/ProfileWebsiteViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileWebsiteViewController.h
+//  Gitos-iPad
+//
+//  Created by Tri Vuong on 2/5/13.
+//  Copyright (c) 2013 Crafted By Tri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileWebsiteViewController : UIViewController
+
+@end
---
+++ Gitos-iPad/Controllers/ProfileWebsiteViewController.m
@@ -0,0 +1,38 @@
+//
+//  ProfileWebsiteViewController.m
+//  Gitos-iPad
+//
+//  Created by Tri Vuong on 2/5/13.
+//  Copyright (c) 2013 Crafted By Tri. All rights reserved.
+//
+
+#import "ProfileWebsiteViewController.h"
+
+@interface ProfileWebsiteViewController ()
+
+@end
+
+@implementation ProfileWebsiteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
