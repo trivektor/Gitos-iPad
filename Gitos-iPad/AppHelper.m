4f5e66ce4aa47530884d776aacf2ee1f3bffa48f
trivektor <trivektor@gmail.com>
2013-04-02 23:03:05 -0700

    add application helpers

---
+++ Gitos-iPad/AppHelper.m
@@ -0,0 +1,13 @@
+//
+//  AppHelper.m
+//  Gitos-iPad
+//
+//  Created by Tri Vuong on 4/2/13.
+//  Copyright (c) 2013 Crafted By Tri. All rights reserved.
+//
+
+#import "AppHelper.h"
+
+@implementation AppHelper
+
+@end
