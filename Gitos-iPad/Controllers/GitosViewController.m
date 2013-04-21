6147a5a5948ce9453a21e70e090677ae84c5bd31
trivektor <trivektor@gmail.com>
2013-04-20 19:19:58 -0700

    add GitosViewController

---
+++ Gitos-iPad/Controllers/GitosViewController.m
@@ -0,0 +1,50 @@
+//
+//  GitosViewController.m
+//  Gitos-iPad
+//
+//  Created by Tri Vuong on 4/20/13.
+//  Copyright (c) 2013 Crafted By Tri. All rights reserved.
+//
+
+#import "GitosViewController.h"
+
+@interface GitosViewController ()
+
+@end
+
+@implementation GitosViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)performHousekeepingTasks
+{
+    UIBarButtonItem *menuButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"399-list1.png"] landscapeImagePhone:nil style:UIBarButtonItemStyleBordered target:self action:@selector(showMenu)];
+    
+    [self.navigationItem setLeftBarButtonItem:menuButton];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)showMenu
+{
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"ToggleViewDeck" object:nil];
+}
+
+@end
