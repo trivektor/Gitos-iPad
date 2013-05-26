f06d4f25e0c4252317be26350fc242bea7ee0d73
trivektor <trivektor@gmail.com>
2013-05-25 20:11:14 -0700

    add empty NewIssueViewController

---
+++ Gitos-iPad/Controllers/Issue/NewIssueViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewIssueViewController.h
+//  Gitos-iPad
+//
+//  Created by Tri Vuong on 5/25/13.
+//  Copyright (c) 2013 Crafted By Tri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewIssueViewController : UIViewController
+
+@end
---
+++ Gitos-iPad/Controllers/Issue/NewIssueViewController.m
@@ -0,0 +1,38 @@
+//
+//  NewIssueViewController.m
+//  Gitos-iPad
+//
+//  Created by Tri Vuong on 5/25/13.
+//  Copyright (c) 2013 Crafted By Tri. All rights reserved.
+//
+
+#import "NewIssueViewController.h"
+
+@interface NewIssueViewController ()
+
+@end
+
+@implementation NewIssueViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
