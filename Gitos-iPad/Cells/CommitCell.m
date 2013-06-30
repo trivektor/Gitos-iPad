2b20f0712de4688adabd55cedea4c258a9d2e410
trivektor <trivektor@gmail.com>
2013-06-29 19:58:21 -0700

    minor font update

--- Gitos-iPad/Cells/CommitCell.m
+++ Gitos-iPad/Cells/CommitCell.m
@@ -40,6 +40,7 @@
 - (void)addShaLabel
 {
     SAMBadgeView *shaLabel = [[SAMBadgeView alloc] initWithFrame:CGRectMake(871, 17, 180, 21)];
+    shaLabel.textLabel.font = [UIFont fontWithName:@"Arial" size:14.0];
     shaLabel.textLabel.text = [[commit getSha] substringToIndex:10];
     shaLabel.textLabel.highlightedTextColor = [UIColor asbestosColor];
     shaLabel.badgeColor = [UIColor silverColor];
