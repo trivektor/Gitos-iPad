c943dca342ca0fb15cd7e8f3c93ed30c9bec4110
trivektor <trivektor@gmail.com>
2013-07-06 22:03:03 -0700

    add class CommitActivity

---
+++ Gitos-iPad/Classes/CommitActivity.h
@@ -0,0 +1,17 @@
+//
+//  CommitActivity.h
+//  Gitos-iPad
+//
+//  Created by Tri Vuong on 7/6/13.
+//  Copyright (c) 2013 Crafted By Tri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CommitActivity : NSObject
+
+@property (nonatomic, strong) NSDictionary *data;
+
+- (id)initWithCommitActivityData:(NSDictionary *)commitActivityData;
+
+@end
---
+++ Gitos-iPad/Classes/CommitActivity.m
@@ -0,0 +1,22 @@
+//
+//  CommitActivity.m
+//  Gitos-iPad
+//
+//  Created by Tri Vuong on 7/6/13.
+//  Copyright (c) 2013 Crafted By Tri. All rights reserved.
+//
+
+#import "CommitActivity.h"
+
+@implementation CommitActivity
+
+@synthesize data;
+
+- (id)initWithCommitActivityData:(NSDictionary *)commitActivityData
+{
+    self = [super init];
+    data = commitActivityData;
+    return self;
+}
+
+@end
